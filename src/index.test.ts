ed1c2cf846ecbb2350b99014ace3c2b062aefc52
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2022-03-22 16:55:17 +0530

    errors

--- src/index.test.ts
+++ src/index.test.ts
@@ -13,4 +13,23 @@ test("to be zero" , ()=>{
     expect(value).not.toBeUndefined();
     expect(value).not.toBeTruthy();   //terminal indicates what and where went wrong; //toBeTruthy --> not.toBrTruthy
     expect(value).toBeFalsy();
-})
+});
+test('to be null', () => {
+    const value = null;
+    expect(value).toBeNull();
+    expect(value).toBeDefined();
+    expect(value).not.toBeUndefined();  //null != undefined
+    expect(value).not.toBeTruthy();
+    expect(value).toBeFalsy();
+  });
+
+
+  //error testing
+  const throwError = ()=>{
+      throw new Error('It doesn\'t works as expected');     //use throw not return
+  }
+
+  test('erorr testing' , ()=>{
+      expect(()=>throwError()).toThrow(Error)  //to only check error is there or not
+      expect(()=>throwError()).toThrow('It doesn\'t works as expected')   //to check exact statement
+  })
