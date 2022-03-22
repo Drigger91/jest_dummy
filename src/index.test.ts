6a86f75f16c06487331444c16f369e9097b9f006
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2022-03-22 18:55:01 +0530

    babel.config.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,14 +4,14 @@ test("add two numbers" , ()=>{
     expect(sum(1,2)).toBe(3)
 });
 
-// //trying different matchers
+////trying different matchers
 
 test("to be zero" , ()=>{
     const value = 0;
     expect(value).not.toBeNull();
     expect(value).toBeDefined();
     expect(value).not.toBeUndefined();
-    expect(value).not.toBeTruthy();   //terminal indicates what and where went wrong; //toBeTruthy --> not.toBrTruthy
+    expect(value).not.toBeTruthy();   //terminal indicates what and where went wrong; //toBeTruthy --> not.toBeTruthy
     expect(value).toBeFalsy();
 });
 test('to be null', () => {
